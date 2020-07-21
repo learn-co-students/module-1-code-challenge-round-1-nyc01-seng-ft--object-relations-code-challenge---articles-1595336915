d4d04017dd9fafaf97a5019a045b489ef314551d
junko911 <junkotahara911@gmail.com>
2020-07-21 10:01:32 -0400

    Created self.all method.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,15 @@
 class Article
   attr_reader :title
+  @@all = []
 
   def initialize(author, magazine, title)
     @author = author
     @magazine = magazine
     @title = title
+    Article.all << self
+  end
+
+  def self.all
+    @@all
   end
 end
