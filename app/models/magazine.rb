88b4f43af3d646acb5dae3ff59612e83b6fff624
humorous-storage-7521 <rmwerner@umich.edu>
2020-07-21 11:04:14 -0400

    Final

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,49 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    Magazine.all << self
+  end
+  
+  def self.all
+    @@all
   end
 
+  def articles
+    Article.all.select {|article| article.magazine == self}
+  end
 
+  def contributors
+    articles.map {|article| article.author}.uniq
+  end
+
+  def self.find_by_name(name)
+    Magazine.all.find {|magazine| magazine.name == name}
+  end
+
+  def article_titles
+    articles.map {|article| article.title}
+  end
+
+  def article_count_by_author
+    count_hash = {}
+    articles.each do |article|
+      count_hash[article.author] ||= 0
+      count_hash[article.author] += 1
+    end
+    count_hash
+  end
+  
+  def contributing_authors
+    authors_array = []
+    article_count_by_author.each do |author, count| 
+      if count > 2
+        authors_array << author
+      end
+    end
+    authors_array
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,9 +8,29 @@ end
 ###  WRITE YOUR TEST CODE HERE ###
 
 
+rock_music = Magazine.new("Rock Weekly", "Music")
+pop_music = Magazine.new("Pop Weekly", "Music")
+time = Magazine.new("Time", "Current Events")
+psychology = Magazine.new("Psychology Today", "Science/Technology")
 
+bob = Author.new("Bob")
+jane = Author.new("Jane")
+mike = Author.new("Mike")
+amy = Author.new("Amy")
+sheldon = Author.new("Sheldon")
 
-
+qotsa = Article.new(bob, rock_music, "Queens of the Stone Age Review")
+raconteurs = Article.new(jane , rock_music, "Raconteurs Review")
+guitar = Article.new(bob, rock_music, "Has Electric Guitar Gone Out Of Style?")
+zepp = Article.new(bob, rock_music, "New Led Zeppelin Bootleg")
+guitar = Article.new(mike, rock_music, "Has Electric Guitar Gone Out Of Style?")
+new_artists = Article.new(jane, rock_music, "Must See New Artists")
+grammys = Article.new(jane, rock_music, "Why Are There No Rock Bands at the Grammys?")
+one_d = Article.new(bob, pop_music, "Harry Styles: Off the Stage")
+neuro_stuff = Article.new(amy, psychology, "New Neuroscience Stuff")
+more_neuro_stuff = Article.new(bob, psychology, "More New Neuroscience Stuff")
+love = Article.new(amy, psychology, "Why I Love Sheldon")
+life_hacks = Article.new(amy, psychology, "10 New Life Hacks To Try")
 
 
 
