5246e6a7f0e6ea9c37c64b9e495f747d17effcfe
pairofAces <karanchauhan16@gmail.com>
2020-07-21 09:40:07 -0400

    first commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,17 @@
 class Article
+    attr_reader :magazine, :author, :title
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        Article.all << self
+    end
+
+    def self.all
+        @@all
+    end
 
 
 end
