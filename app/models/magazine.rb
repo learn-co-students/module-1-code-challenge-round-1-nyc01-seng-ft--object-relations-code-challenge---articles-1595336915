457a8c946ade2470c8d272233f122df0aec46b4b
irosen419 <irosen419@gmail.com>
2020-07-21 10:57:31 -0400

    Completed Mod1 Codee Challenge

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,41 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select {|article| article.magazine == self}
+  end
+
+  def non_uniq_contributors
+    articles.map {|article| article.author}
+  end
+
+  def contributors
+    non_uniq_contributors.uniq
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|mag| mag.name == name}
+  end
+
+  def article_titles
+    articles.map {|article| article.title}
+  end
+
+  def contributing_authors
+    num_of_articles = non_uniq_contributors.each_with_object(Hash.new(0)) {|contributor, counts| counts[contributor] += 1}
+    num_of_articles.map {|author, num| author if num > 2}.compact
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,8 +7,24 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+#authors
+ian = Author.new("Ian")
+tashawn = Author.new("Tashawn")
+
+#magazines
+pets_mag = Magazine.new("Pets Magazine", "Pets")
+drums_mag = Magazine.new("Drums Magazine", "Musical Instruments")
+pets_mag_2 = Magazine.new("Pets Magazine", "More Pets")
 
 
+#articles
+feeding_pets = Article.new(ian, pets_mag, "How to Feed Your Pets")
+grooming_snakes = Article.new(ian, pets_mag, "How to Groom You Snake Named Bob")
+walk_pets = Article.new(ian, pets_mag, "Where to Walk Your Iguana")
+drum_heads = Article.new(ian, drums_mag, "How to Tighten Your Drum Heads")
+cohorts = Article.new(tashawn, pets_mag, "Training Your Cohort of CodeNewbies")
+early_risers = Article.new(tashawn, pets_mag, "Waking Up Three Hours Ahead of Your Students")
+teaching = Article.new(tashawn, pets_mag, "How to Be A Great Instructor ;)")
 
 
 
