c321c9fbcb544713abd5e46201fd5f308a0e6cae
Teddy Bradsher <teddybradsher@gmail.com>
2020-07-21 14:29:59 -0400

    Finished code challenge

--- tools/console.rb
+++ tools/console.rb
@@ -7,14 +7,19 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+#articles
+verge_pc_build = Article.new(teddy, "verge_magazine", "how to build a cheap 2000 dollar system" )
+world_domination = Article.new(doom, "Latvaria weekly", "what pinky and the brain can teach us about perserverance")
+classmates = Article.new(israel, "example magazine", "hello world"
+#magazines
 
 
 
+#authors
 
-
-
-
-
+# teddy = Author.new("Theodore Bradsher")
+# israel = Author.new("Israel Lastname")
+# doom = Author.new("Dr. DOOM")
 ### DO NOT REMOVE THIS
 binding.pry
 
