8ad17ac2060a4f33833f119d28e9275ea07119d1
dkim61 <dkim187@binghamton.edu>
2020-07-21 11:21:21 -0400

    david kim

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,49 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select {|article| article.magazine == self}
+  end
+# user has many contributers (authors), through articles
+  def contributers
+    self.articles.map {|article| article.author}
+  end
+
+  def self.find_by_name(magazine_name)
+    @@all.select {|article| article.name == magazine_name}
+  end
+
+  def article_titles
+    self.articles.map {|article| article.title}
+  end
+
+  def contributing_authors
+    authors_with_articles = []
+    self.contributers.each do |contributer|
+      if !self.contributer.uniq.includes? (contributer)
+        authors_with_articles << (contributer)
+      end
+      # Not sure if bang is used corrently here, before I had it this way
+      # (I am unsure how to use bang operator)
+      #
+      # if self.contributer.uniq.includes? (contributer)
+      # else
+      # authors_with_articles << (contributer)
+      # end
+    end
+    authors_with_articles.uniq
+  end
 
 end
---
+++ app/models/run.rb
@@ -0,0 +1,6 @@
+require 'pry'
+
+require_relative '../app/author.rb'
+require_relative '../app/aritcle.rb'
+require_relative '../app/magazine.rb'
+
