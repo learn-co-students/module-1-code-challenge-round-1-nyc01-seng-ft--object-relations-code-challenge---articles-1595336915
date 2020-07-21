6f494ebbce1aad2d408ec778c985766cdf469b1f
github username <github email address>
2020-07-21 11:21:51 -0400

    Israel Canessa

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,21 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def articles
+    Articles.all.select do |article|
+      article.author == self
+  end
+  
+  def magazines
+    self.articles.map do |article|
+      article.magazine.uniq
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,21 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+##Author
+a1 = Author.new("Israel Canessa")
+a2 = Author.new("Mario Casas")
+a3 = Author.new("Morgan Freeman")
+a4 = Author.new("Emma Watson")
+a5 = Author.new("Anais Villegas")
 
+
+##Magazine
+m1 = Magazine.new("Tech", "Technology")
+
+m2 = Magazine.new("Miracles", "Religion")
+m3 = Magazine.new("NASCAR", "Sports")
+m4 = Magazine.new("Fashion Week Catalog", "Fashion")
+m5 = Magazine.new("Travel to Japan Now!", "Travel")
 
 
 
