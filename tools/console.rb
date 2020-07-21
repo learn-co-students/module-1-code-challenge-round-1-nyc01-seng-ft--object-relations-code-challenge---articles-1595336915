b28cb1bd1fcfb0fb12f6f8cfeedd6b7b3e7685d6
vincentyang01 <vincentyangcsi@gmail.com>
2020-07-21 09:40:17 -0400

    First commit

--- tools/console.rb
+++ tools/console.rb
@@ -6,7 +6,11 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
+author1 = Author.new("apple")
+author2 = Author.new("banana")
+author3 = Author.new("orange")
+author4 = Author.new("peach")
+author5 = Author.new("grape")
 
 
 
