c27854de12d33b3dde088f74ab3164c53c4f8a7c
Alex <alex.beciana@gmail.com>
2020-07-21 11:13:10 -0400

    Second Commit

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,63 @@
+require "pry"
+
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def articles
+    Article.all.select do |article|
+      if article.magazine == self
+        article
+      end
+    end
+  end
+
+  def contributors
+    articles.collect do |article|
+      article.author
+    end.uniq
+  end
+
+  # def matching_author_article
+  #   contributors.select do |author|
+  #     author.articles.select do |article| 
+  #       article.magazine == self
+  #     end
+  #   end
+  # end
+
+  def self.find_by_name(name)
+    self.all.find do |zine|
+      if zine.name == name
+        zine
+      end
+    end
+  end
+
+  def article_titles
+    articles.collect do |article|
+      article.title
+    end
+  end
+
+  def contributing_authors
+    contributors.select do |author|
+      if author.articles.select { |article| article.magazine == self}.count > 2
+        # binding.pry 
+        author
+      end
+    end
+  end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,19 +1,51 @@
-require_relative '../config/environment.rb'
+# require_relative '../config/environment.rb' -- require_all wasn't working properly, giving errors
+require_relative '../app/models/author.rb'
+require_relative '../app/models/magazine.rb'
+require_relative '../app/models/article.rb'
 
-def reload
-  load 'config/environment.rb'
-end
+# def reload -- require_all wasn't working properly, giving errors
+#   load 'config/environment.rb'
+# end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+# (name)
+author1 = Author.new("Hemingway")
+author2 = Author.new("Twain")
+author3 = Author.new("Orwell")
 
+# (name, category)
+zine1 = Magazine.new("Time", "real")
+zine2 = Magazine.new("Fiction", "fiction")
 
+# (author, magazine, title)
+article1 = Article.new(author1, zine1, "cuba")
+article2 = Article.new(author2, zine2, "fantasy")
+article3 = Article.new(author1, zine2, "boats")
+article4 = Article.new(author1, zine2, "stuff")
+article5 = Article.new(author1, zine2, "being a writer")
+article6 = Article.new(author2, zine1, "wlghjarof")
+article7 = Article.new(author2, zine1, "alihnorahfa")
+article8 = Article.new(author3, zine1, "big brother")
 
+author1.articles
 
+author1.magazines
 
+zine1.articles
 
+zine2.contributors
 
+author2.add_article(zine1, "life")
+
+author2.topic_areas
+
+Magazine.find_by_name("Time")
+
+zine2.article_titles
+
+zine2.contributing_authors
 
 ### DO NOT REMOVE THIS
 binding.pry
